b3e776f17f54b14b6b3c22d25ade7674bfe682ee
Giovanni <me@giovanni.com>
2016-12-24 20:23:48 +0100

    comment in the sidebar helper

--- client/templates/sidebar/sidebar.js
+++ client/templates/sidebar/sidebar.js
@@ -2,4 +2,6 @@ Template.sidebar.helpers({
   'categories':function(){
     return Category.find();
   }
+  // because in the template we used the word "categories" in the #each cycle.
+  // we now have to tell what that categories is
 });
